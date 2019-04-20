58ba4fb40f861b68787b506a05ea0c65ede3ecf2
Jonathan Dorety <kyullog@gmail.com>
2019-04-19 20:45:04 -0400

    Create first add user endpoint test

--- api/server.test.js
+++ api/server.test.js
@@ -1,9 +1,21 @@
 const server = require("./server.js");
 const request = require("supertest");
+const db = require("../data/dbConfig.js");
 
 describe("The Server", () => {
   it("is listening and responding", async () => {
     const res = await request(server).get("/");
     expect(res.status).toBe(200);
+  });
+  describe("POST /users", () => {
+    beforeEach(() => {
+      return db("users").truncate();
+    });
+
+    it("should create a new user and return the id/name", async () => {
+      const testUser = { name: "Jonathan" };
+      const res = await request(server).post("/users", testUser);
+      expect(res.body).toEqual(testUser);
+    });
   });
 });
