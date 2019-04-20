e682acab34c9f850a0eb3848c10cdab6d6056139
Jonathan Dorety <kyullog@gmail.com>
2019-04-19 22:26:49 -0400

    Delete endpoint function successfully passes both tests

--- api/server.js
+++ api/server.js
@@ -21,7 +21,11 @@ server.post("/users/:id/delete", async (req, res) => {
   const id = req.params.id;
   try {
     const deleted = await Users.remove(id);
-    res.status(200).json({ message: "User deleted" });
+    if (deleted) {
+      res.status(200).json({ message: "User deleted" });
+    } else {
+      res.status(404).json({ err: "No user by that id exists" });
+    }
   } catch (err) {
     res.status(500).json({ err: "There was a problem deleting the user" });
   }
